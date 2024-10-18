345e04ce3c02b921c3a2f771c459192d6556b144
noeltiju <noelab04@gmail.com>
2024-10-18 19:03:05 +0530

    Updated the win, draw percentages as features

--- helper_files/draw_percentage_maker.py
+++ helper_files/draw_percentage_maker.py
@@ -2,12 +2,12 @@ import pandas as pd
 import csv
 
 
-dataset = pd.read_csv('/Users/noeltiju/Desktop/ML-Project/ELO-Insights/training_dataset.csv')
+dataset = pd.read_csv('../Datasets/training_dataset.csv')
 dataset = dataset.reset_index()
 
 team_data = {}
 
-with open('updated_training_data.csv', 'w', newline="") as my_file:
+with open('../Datasets/updated_training_data.csv', 'w', newline="") as my_file:
     writer = csv.writer(my_file)
     writer.writerow(['Home Team', 'Away Team', 'Home Team ELO', 'Away Team ELO', 'Home Team Win Percentage', 'Home Team Draw Percentage', 'Away Team Win Percentage', 'Away Team Draw Percentage', 'Winner'])
     
@@ -25,6 +25,11 @@ with open('updated_training_data.csv', 'w', newline="") as my_file:
     for index, row in dataset.iterrows():
         home_team = row['Home Team']
         away_team = row['Away Team']
+
+        home_win_percentage = round((team_data[home_team][0] / team_data[home_team][2]) * 100, 2) if team_data[home_team][2] > 0 else 0
+        home_draw_percentage = round((team_data[home_team][1] / team_data[home_team][2]) * 100, 2) if team_data[home_team][2] > 0 else 0
+        away_win_percentage = round((team_data[away_team][0] / team_data[away_team][2]) * 100, 2) if team_data[away_team][2] > 0 else 0
+        away_draw_percentage = round((team_data[away_team][1] / team_data[away_team][2]) * 100, 2) if team_data[away_team][2] > 0 else 0
 
         if row['Winner'] == 1:
 
@@ -46,14 +51,6 @@ with open('updated_training_data.csv', 'w', newline="") as my_file:
             team_data[away_team][2] += 1
 
             team_data[home_team][2] += 1
-
-
-        home_win_percentage = round((team_data[home_team][0] / team_data[home_team][2]) * 100, 2) if team_data[home_team][2] > 0 else 0
-        home_draw_percentage = round((team_data[home_team][1] / team_data[home_team][2]) * 100, 2) if team_data[home_team][2] > 0 else 0
-        away_win_percentage = round((team_data[away_team][0] / team_data[away_team][2]) * 100, 2) if team_data[away_team][2] > 0 else 0
-        away_draw_percentage = round((team_data[away_team][1] / team_data[away_team][2]) * 100, 2) if team_data[away_team][2] > 0 else 0
-
-
 
         writer.writerow([home_team, away_team, row['Home_Team_Rating'], row['Away_Team_Rating'], home_win_percentage, home_draw_percentage, away_win_percentage, away_draw_percentage, row['Winner']])
 
